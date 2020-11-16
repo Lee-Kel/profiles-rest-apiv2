aa510409e60cc13b01ae4dcbb5cda37844cf5e27
Lee Kellet <leekellet@hotmail.com>
2020-11-16 20:21:58 +0000

    Added user profile model, manager and maigrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -11,13 +11,14 @@ class UserProfileManager(BaseUserManager):
             raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, name=name)
+        user = self.model(email=email, name=name,)
+
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_super_user(self, email, name, password):
+    def create_superuser(self, email, name, password):
         """Create new administrative user"""
         user = self.create_user(email, name, password)
         user.is_superuser = True
@@ -38,7 +39,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name,']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Get Full Name of User"""
